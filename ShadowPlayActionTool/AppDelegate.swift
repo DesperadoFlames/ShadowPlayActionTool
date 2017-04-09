5b65e4802911dcb8c80e97d04219397858158420
Desperado <flamesdesperado@gmail.com>
2017-04-09 14:54:35 +0800

    Initial Commit

---
+++ ShadowPlayActionTool/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  ShadowPlayActionTool
+//
+//  Created by Desperado on 4/9/29 H.
+//  Copyright © 29 Heisei Des. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
